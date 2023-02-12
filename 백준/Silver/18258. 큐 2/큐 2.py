8fa54e7e0600696a9dac746f27b051557d9d26db
joyful-young <119635748+joyful-young@users.noreply.github.com>
2023-02-12 15:52:25 +0900

    [Silver IV] Title: 큐 2, Time: 1752 ms, Memory: 110612 KB -BaekjoonHub

---
+++ 백준/Silver/18258. 큐 2/큐 2.py
@@ -0,0 +1,36 @@
+# 백준 18258. 큐2
+# deque 안쓰고
+
+import sys
+from collections import deque
+
+N = int(sys.stdin.readline())
+
+queue = deque()
+
+for _ in range(N):
+    cmd = sys.stdin.readline().strip()
+    if cmd == 'pop':
+        if not queue:
+            print(-1)
+        else:
+            print(queue.popleft())
+    elif cmd == 'size':
+        print(len(queue))
+    elif cmd == 'empty':
+        if queue:
+            print(0)
+        else:
+            print(1)
+    elif cmd == 'front':
+        if not queue:
+            print(-1)
+        else:
+            print(queue[0])
+    elif cmd == 'back':
+        if not queue:
+            print(-1)
+        else:
+            print(queue[-1])
+    else:
+        queue.append(int(cmd.split()[1]))
