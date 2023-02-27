6b65e48418058aedd0dfda04ec0486e989ce56b7
joyful-young <119635748+joyful-young@users.noreply.github.com>
2023-02-27 20:55:09 +0900

    [Gold II] Title: 가운데를 말해요, Time: 204 ms, Memory: 37268 KB -BaekjoonHub

--- 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.py
+++ 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.py
@@ -11,16 +11,17 @@ min_heap = []   # 오른쪽
 
 for _ in range(N):
     tmp = int(input())
-    if len(max_heap) == len(min_heap):      # 양쪽에 똑같이 들어있으면 왼쪽에 넣기
-        heapq.heappush(max_heap, -tmp)      # 최대힙이므로 - 붙여서 넣기
+    if len(max_heap) == len(min_heap):      # 양쪽에 똑같이 들어있으면 왼쪽에 넣어야.
+        if min_heap and tmp > min_heap[0]:      # 오른쪽 최솟값보다 큰 수이면
+            heapq.heappush(max_heap, -heapq.heappop(min_heap))  # 오른쪽 최솟값 빼다가 왼쪽에 넣고
+            heapq.heappush(min_heap, tmp)       # 오른쪽에 tmp 넣기
+        else:       # 오른쪽 최솟값보다 크지 않으면
+            heapq.heappush(max_heap, -tmp)      # 왼쪽에 넣기
+    else:       # 왼쪽에 더 많이 들어있을 때는 오른쪽에 넣어야
+        if max_heap and -max_heap[0] > tmp:     # 왼쪽 최댓값보다 tmp가 더 작은 값이면 왼쪽에 넣어야.
+            heapq.heappush(min_heap, -heapq.heappop(max_heap))  # 왼쪽 최댓값 빼다가 오른쪽에 넣고
+            heapq.heappush(max_heap, -tmp)      # 왼쪽에 tmp 넣기
+        else:       # tmp가 왼쪽 최댓값 이상이면 오른쪽에 넣으면 됨
+            heapq.heappush(min_heap, tmp)
 
-    else:                                   # 오른쪽이 더 적으면 오른쪽에 넣기
-        heapq.heappush(min_heap, tmp)       # 최소힙
-
-    if max_heap and min_heap and -max_heap[0] > min_heap[0]:     # 집어넣었을 때 왼쪽 힙 최댓값이 오른쪽 힙 최솟값보다 크면
-        left = -heapq.heappop(max_heap)      # 왼쪽에서 빼냄
-        right = heapq.heappop(min_heap)     # 오른쪽에서 빼냄
-        heapq.heappush(max_heap, -right)
-        heapq.heappush(min_heap, left)
-
-    print(-max_heap[0])     # 왼쪽 힙의 루트 값이 중간값
+    print(-max_heap[0])
