68db157f5696a04e7b8595924285e9737fd22eaa
joyful-young <119635748+joyful-young@users.noreply.github.com>
2023-03-02 10:00:19 +0900

    [Silver IV] Title: 수 찾기, Time: 412 ms, Memory: 46736 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,34 +1,31 @@
-# 백준 1920 수 찾기
+# 백준 1920. 수 찾기
 
-N = int(input())
-n_lst = list(map(int, input().split()))
-
-M = int(input())
-m_lst = list(map(int, input().split()))
-
-# n_lst에 m_lst의 수가 존재하면 1, 없으면 0
-# 이분 탐색 이용을 위해 정렬
-n_lst.sort()
+import sys
+input = sys.stdin.readline
 
 
-
-# m_lst 숫자 하나씩 찾아봄
-for m in m_lst:
-    left = 0
-    right = N - 1
+def binary_search(x):
+    left = 0    # 왼쪽 끝 인덱스
+    right = N - 1   # 오른쪽 끝 인덱스
 
     while left <= right:
-        mid = (left + right) // 2
-        # 중간 값이 딱 찾는 값
-        if n_lst[mid] == m:
-            print(1)
-            break
-        # 찾는 값이 중간의 값보다 작으면 왼쪽을 더 찾아봐야 -> 오른쪽 끝 변경
-        elif m < n_lst[mid]:
+        mid = (left + right) // 2   # 중간 인덱스
+        if x == arr[mid]:   # 찾는 값과 일치하면
+            return 1
+        elif x < arr[mid]:      # 찾는 값보다 작으면 왼쪽 구간 다시 탐색
             right = mid - 1
-        # 찾는 값이 중간의 값보다 크면 오른쪽을 더 찾아봐야 -> 왼쪽 끝 변경
-        else:
+        else:                   # 찾는 값보다 크면 오른쪽 구간 다시 탐색
             left = mid + 1
-    # while문 다 돌았는데 못찾았으면 없는 것
-    else:
-        print(0)
+    return 0
+
+
+N = int(input())
+
+arr = sorted(list(map(int, input().split())))   # 오름차순 정렬
+
+M = int(input())
+
+numbers = list(map(int, input().split()))
+
+for num in numbers:
+    print(binary_search(num))
