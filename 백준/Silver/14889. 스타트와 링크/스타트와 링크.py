8b8f6400bf11a05675376d35dc7b0ddd1ec027a4
joyful-young <119635748+joyful-young@users.noreply.github.com>
2023-03-04 23:21:36 +0900

    [Silver II] Title: 스타트와 링크, Time: 3388 ms, Memory: 84604 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
@@ -1,11 +1,19 @@
 # 백준 14889. 스타트와 링크 - swea 요리사 문제랑 같음
 # itertools
 
+# import sys
 from itertools import combinations
+# input = sys.stdin.readline
 
 N = int(input())
 
 arr = [list(map(int, input().split())) for _ in range(N)]
+
+# Sij + Sji 합 배열 만들어놓기(j > i인 부분만)
+for i in range(N):
+    for j in range(N):
+        if j > i:
+            arr[i][j] += arr[j][i]
 
 # 사람들 중 N // 2 명 고른 부분집합 만들기
 start_team = list(combinations(range(N), N // 2))
@@ -21,11 +29,11 @@ for k in range(len(start_team)):
 
     # 정해진 스타트 팀에서 두 명 뽑는 조합
     for i, j in combinations(start_team[k], 2):
-        start += arr[i][j] + arr[j][i]      # 팀 능력치 구하기
+        start += arr[i][j]      # 팀 능력치 구하기
 
     # 정해진 링크 팀에서 두 명 뽑는 조합
     for i, j in combinations(link_team[k], 2):
-        link += arr[i][j] + arr[j][i]
+        link += arr[i][j]
 
     minV = min(minV, abs(start - link))
 
