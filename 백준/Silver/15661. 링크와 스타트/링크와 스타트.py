159f4b73708dd7070459e0572c48b566511f37df
joyful-young <119635748+joyful-young@users.noreply.github.com>
2023-03-29 00:17:26 +0900

    [Silver I] Title: 링크와 스타트, Time: 1356 ms, Memory: 115360 KB -BaekjoonHub

--- 백준/Silver/15661. 링크와 스타트/링크와 스타트.py
+++ 백준/Silver/15661. 링크와 스타트/링크와 스타트.py
@@ -1,14 +1,11 @@
 # 백준 15661. 링크와 스타트
-# 팀 인원수는 같지 않아도 됨. 한 명 이상
-# 1 ~ N번 사람
-# 스타트 팀 링크 팀 능력치 차이 최소로
 
 N = int(input())
 arr = [list(map(int, input().split())) for _ in range(N)]
 
 # 스타트 팀에 뽑힌 걸 1, 링크 팀에 뽑힌 걸 0으로 표현
 minV = 40000
-for i in range(1, (1 << N) - 1):  # 모두 다 링크 팀 제외, 모두 다 스타트팀 제외
+for i in range(1, (1 << (N - 1))):  # 모두 다 링크 팀 제외, 모두 다 스타트팀 제외
     # 숫자 i가 정해지면 팀이 정해짐
     tmp = 0     # 두 팀의 차이
     for j in range(N - 1):      # 정해진 팀에서 j번째, k번째 사람 두명 조합
@@ -20,4 +17,3 @@ for i in range(1, (1 << N) - 1):  # 모두 다 링크 팀 제외, 모두 다 스
     minV = min(minV, abs(tmp))      # 두 팀 능력치 차이 최솟값으로 갱신
 
 print(minV)
-
