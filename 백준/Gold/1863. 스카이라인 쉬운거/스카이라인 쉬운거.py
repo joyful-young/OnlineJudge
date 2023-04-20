cd339b1ebaa2138cc38b3d4d08fb45cd1c459310
joyful-young <119635748+joyful-young@users.noreply.github.com>
2023-04-20 13:39:42 +0900

    [Gold V] Title: 스카이라인 쉬운거, Time: 60 ms, Memory: 33300 KB -BaekjoonHub

--- 백준/Gold/1863. 스카이라인 쉬운거/스카이라인 쉬운거.py
+++ 백준/Gold/1863. 스카이라인 쉬운거/스카이라인 쉬운거.py
@@ -3,23 +3,21 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-stack = []
 cnt = 0
 arr = [list(map(int, input().split())) for _ in range(n)] + [[1000000, 0]]
 
-for i in range(n + 1):
+stack = [arr[0][1]]
+
+for i in range(1, n + 1):
     h = arr[i][1]
-    if not stack:
+    if stack[-1] < h:       # 보이는 높이보다 낮은 건물들은 아직 있을 수 있음
         stack.append(h)
     else:
-        if stack[-1] < h:
-            stack.append(h)
-        else:
-            while stack and stack[-1] > h:
-                stack.pop()
-                cnt += 1
-            if stack and stack[-1] == h:
-                stack.pop()
-            stack.append(h)
+        while stack and stack[-1] > h:      # 보이는 높이가 낮아졌다는 건 그보다 위에 있는 건물 윤곽은 끝난 것
+            stack.pop()
+            cnt += 1        # 높은 건물 빼서 개수 세기
+        if stack and stack[-1] == h:        # 보이는 높이와 같은 건물은 아직 더 이어질 수 있음
+            stack.pop()
+        stack.append(h)
 
 print(cnt)
