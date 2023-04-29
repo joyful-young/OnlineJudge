e9e5e5f61dae96a5578d9be705c5749718b9fe82
joyful-young <119635748+joyful-young@users.noreply.github.com>
2023-04-29 18:44:13 +0900

    [unrated] Title: 코드 처리하기, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.js
@@ -0,0 +1,24 @@
+function solution(code) {
+    var answer = '';
+    let mode = 0
+    for (let idx = 0; idx < code.length; idx++) {
+        if (mode === 0) {
+            if (code[idx] === '1') {
+                mode = 1
+            } else {
+                if (idx % 2 == 0) {
+                    answer += code[idx]   
+                }
+            }
+        } else {
+            if (code[idx] === '1') {
+                mode = 0
+            } else {
+                if (idx % 2) {
+                    answer += code[idx]
+                }
+            }
+        }
+    }
+    return answer != '' ? answer: 'EMPTY'
+}
