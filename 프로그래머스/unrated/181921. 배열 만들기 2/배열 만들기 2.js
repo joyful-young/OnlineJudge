36405e83a80d8f84f2ff32eff30861a0aaf51507
joyful-young <119635748+joyful-young@users.noreply.github.com>
2023-04-29 19:23:40 +0900

    [unrated] Title: 배열 만들기 2, Time: 29.02 ms, Memory: 40.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181921. 배열 만들기 2/배열 만들기 2.js
@@ -0,0 +1,22 @@
+function solution(l, r) {
+    var answer = [];
+    const zero_five = new Set(['0', '5'])
+    
+    Set.prototype.isSuperset = function(subset) {
+    for (var elem of subset) {
+        if (!this.has(elem)) {
+            return false;
+        }
+    }
+    return true;
+    }
+    
+    
+    for (let num = l; num < r + 1; num++) {
+        const set = new Set(String(num))
+        if (zero_five.isSuperset(set)) {
+            answer.push(num)
+        }
+    }
+    return answer.length === 0 ? [-1] : answer
+}
