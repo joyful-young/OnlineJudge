088d4c5dd1e21ea076b561aa1a2bd0b4fb28c7c6
joyful-young <119635748+joyful-young@users.noreply.github.com>
2023-06-16 08:58:31 +0900

    [Silver II] Title: DFS와 BFS, Time: 444 ms, Memory: 32388 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,46 +1,41 @@
-# 백준 1260. DFS와 BFS
-
-# 정점 번호 작은 것을 먼저 방문
-
-N, M, V = map(int, input().split())
-
-graph = [[] for _ in range(N + 1)]
-
-for _ in range(M):
-    node1, node2 = map(int, input().split())
-    graph[node1].append(node2)
-    graph[node2].append(node1)
-
-# 작은 숫자가 먼저 나오게 정렬
-for i in range(N + 1):
-    graph[i].sort()
-
-visited_dfs = [False] * (N + 1)
-
-
+# 재귀
 def dfs(v, visited):
-    visited[v] = True
+    visited[v] = 1
     print(v, end=' ')
-    for next_node in graph[v]:
-        if not visited[next_node]:
-            dfs(next_node, visited)
+    for w in adjL[v]:
+        if not visited[w]:
+            dfs(w, visited)
 
 
-def bfs(v):
+def bfs(v, visited):
+    # 시작점 인큐
     queue = [v]
-    visited = [False] * (N + 1)
-    visited[v] = True
-    print(v, end=' ')
+    visited[v] = 1
 
     while queue:
-        node = queue.pop(0)
-        for next_node in graph[node]:
-            if not visited[next_node]:
-                visited[next_node] = True
-                queue.append(next_node)
-                print(next_node, end=' ')
+        v = queue.pop(0)
+        print(v, end=' ')
+        # 현재 정점에 인접한 정점 중 방문하지 않았던 곳 방문
+        for w in adjL[v]:
+            if not visited[w]:
+                queue.append(w)
+                visited[w] = 1
+        # 인접한 곳 다 방문했으면 처음으로 돌아가서 큐에서 정점 꺼내기
 
 
-dfs(V, visited_dfs)
+N, M, V = map(int, input().split())     # 정점 개수, 간선 개수, 탐색 시작 정점
+adjL = [[] for _ in range(N + 1)]   # 정점 번호 1 ~ N
+for _ in range(M):
+    v1, v2 = map(int, input().split())
+    adjL[v1].append(v2)
+    adjL[v2].append(v1)
+
+for i in range(N + 1):
+    adjL[i].sort()
+
+
+visited = [0 for _ in range(N + 1)]
+dfs(V, visited)
 print()
-bfs(V)
+visited = [0 for _ in range(N + 1)]
+bfs(V, visited)
