8dde9d55667cdccf9e473cc7a269736779fe2f68
joyful-young <119635748+joyful-young@users.noreply.github.com>
2023-06-16 14:40:36 +0900

    [Silver I] Title: 회의실 배정, Time: 4276 ms, Memory: 52036 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.py
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -1,21 +1,38 @@
-# 1931 백준 회의실 , 그리디
+# 백준 1931. 회의실 배정
+
 N = int(input())
-li = []
-for tc in range(N):
-    x, y = list(map(int,input().split()))
-    li.append([x,y])
 
-sor = sorted(li, key=lambda x: (x[1],x[0])) # 시작시간 오름차순때리고 끝나는시간 오름차순때림
+arr = [0] * N
 
+for i in range(N):
+    arr[i] = list(map(int, input().split()))
 
-start = sor[0][0]
-end = sor[-1][-1]
+# print(arr)
 
-time = [[sor[0][0],sor[0][1]]] # 최대가능한 회의시간
+arr.sort(key=lambda x: x[0])    # 하위 우선순위
+arr.sort(key=lambda x: x[1])    # 상위 우선순위
 
-end_time = sor[0][1] #끝시간 세팅했음 ~
-for i in range(1, len(sor)): # 젤 처음꺼 넣어놨따
-    if end_time <= sor[i][0]:
-        time.append([sor[i][0],sor[i][1]])
-        end_time = sor[i][1]
-print(len(time))
+# print(arr)
+
+# # 틀려서 새로 추가한 부분
+# # 첫 회의 찾기 - 가장 빠른 시간에 끝나는 것
+# tmp = arr[0][1]     # 가장 빠른 종료 시간
+# min_idx = 0
+# for i in range(N):
+#     if arr[i][1] != tmp:    # 종료시간 다르면 break
+#         break
+#     if arr[i][0] < arr[min_idx][0]:     # 종료시간 같고 시작시간 더 빠른 게 있으면 그것부터
+#         min_idx = i
+
+# 첫 회의
+m = arr[0]
+cnt = 1
+
+i = 1       # i 초기화
+while i < N:
+    if arr[i][0] >= m[1]:       # 회의 시작 시간이 이전 회의 종료시간 이상이면
+        cnt += 1
+        m = arr[i]              # 그게 다음 회의
+    i += 1
+
+print(cnt)
