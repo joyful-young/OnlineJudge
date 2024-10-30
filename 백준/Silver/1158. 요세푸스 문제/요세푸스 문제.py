adb49ba1231be810c7885e4fd4732afda0d53d5a
joyful-young <119635748+joyful-young@users.noreply.github.com>
2024-10-30 15:40:46 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
@@ -1,17 +1,11 @@
 # 1158. 요세푸스 문제
-import sys
-from collections import deque
-input = sys.stdin.readline
-
-N, K = map(int, input().strip().split())
-numbers = deque([str(i) for i in range(1, N + 1)])
+N, K = map(int, input().split())
+numbers = [str(i) for i in range(1, N + 1)]
+idx = 0
 ans = []
 while numbers:
-    for i in range(K):
-        if i == K - 1:
-            ans.append(numbers.popleft())
-        else:
-            numbers.append(numbers.popleft())
-
+    idx += (K - 1)
+    idx %= len(numbers)
+    ans.append(numbers.pop(idx))
 ret = "<" + ", ".join(ans) + ">"
 print(ret)
