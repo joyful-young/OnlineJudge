548ae7a7cb1552c0a5e4e6b51469e50cc6f11c06
JaeYoung Park <119635748+joyful-young@users.noreply.github.com>
2025-01-28 20:40:28 +0900

    [Gold III] Title: 욕심쟁이 판다, Time: 900 ms, Memory: 54004 KB -BaekjoonHub

---
+++ 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.py
@@ -0,0 +1,31 @@
+# 1937. 욕심쟁이 판다
+import sys
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
+
+def dfs(row, col):
+    if dp[row][col] != -1:
+        return dp[row][col]
+
+    bamboo = arr[row][col]
+    max_value = 1
+    for di, dj in [[-1, 0], [1, 0], [0, -1], [0, 1]]:
+        xi, xj = row + di, col + dj
+        if 0 <= xi < n and 0 <= xj < n and arr[xi][xj] > bamboo:
+            max_value = max(max_value, dfs(xi, xj) + 1)
+    dp[row][col] = max_value
+    return dp[row][col]
+
+
+n = int(input())
+arr = [list(map(int, input().split())) for _ in range(n)]
+dp = [[-1 for _ in range(n)] for _ in range(n)]
+# dp[i][j] i행 j열로부터 이동 가능한 칸의 수 최댓값
+
+ans = 1
+for i in range(n):
+    for j in range(n):
+        ans = max(ans, dfs(i, j))
+
+# print(dp)
+print(ans)
