f6462a98c077462bf7bba5268dcfa47d8d7c4a7b
JaeYoung Park <119635748+joyful-young@users.noreply.github.com>
2025-02-03 09:50:48 +0900

    [Gold III] Title: 욕심쟁이 판다, Time: 624 ms, Memory: 52644 KB -BaekjoonHub

--- 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.py
+++ 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.py
@@ -1,32 +1,52 @@
 # 1937. 욕심쟁이 판다
 import sys
-sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
-
-def dfs(row, col):
-    if dp[row][col] != -1:
-        return dp[row][col]
-
-    bamboo = arr[row][col]
-    max_value = 1
-    for di, dj in [[-1, 0], [1, 0], [0, -1], [0, 1]]:
-        xi, xj = row + di, col + dj
-        if 0 <= xi < n and 0 <= xj < n and arr[xi][xj] > bamboo:
-            max_value = max(max_value, dfs(xi, xj) + 1)
-    dp[row][col] = max_value
-    return dp[row][col]
 
 
 n = int(input())
 arr = [list(map(int, input().split())) for _ in range(n)]
 dp = [[-1 for _ in range(n)] for _ in range(n)]
-# dp[i][j] i행 j열로부터 이동 가능한 칸의 수 최댓값
+# dp[i][j]: i행 j열로부터 이동 가능한 칸의 수 최댓값
+
+directions = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+
+
+def dfs(si, sj):
+    stack = [[si, sj, 0, 1]]    # 행, 열, 이동 방향 인덱스, 현재까지 계산된 이동 가능 칸수 최댓값
+
+    while stack:
+        r, c, dr, current_max = stack[-1]
+
+        if dp[r][c] != -1:  # dp값 이미 결정. 이전 칸 업데이트
+            stack.pop()
+            if stack:
+                stack[-1][3] = max(stack[-1][3], dp[r][c] + 1)
+            continue
+
+        if dr < 4:  # 아직 확인하지 않은 방향 남았을 경우
+            stack[-1][2] += 1
+            ni, nj = r + directions[dr][0], c + directions[dr][1]
+
+            if 0 <= ni < n and 0 <= nj < n and arr[ni][nj] > arr[r][c]:
+                if dp[ni][nj] != -1:
+                    stack[-1][3] = max(stack[-1][3], dp[ni][nj] + 1)
+                else:
+                    stack.append([ni, nj, 0, 1])
+        else:   # 모든 방향 확인 완료. dp 값 결정
+            dp[r][c] = stack[-1][3]
+            stack.pop()
+            if stack:
+                stack[-1][3] = max(stack[-1][3], dp[r][c] + 1)
+
+    return dp[si][sj]
+
 
 ans = 1
 for i in range(n):
     for j in range(n):
         if dp[i][j] == -1:
-            ans = max(ans, dfs(i, j))
+            dfs(i, j)
+        ans = max(ans, dp[i][j])
 
 # print(dp)
 print(ans)
