f474aeb52048e5f20c1dbe9098476e443f5b5cda
JaeYoung Park <119635748+joyful-young@users.noreply.github.com>
2025-02-26 22:15:01 +0900

    [level 0] Title: 수 조작하기 2, Time: 1.51 ms, Memory: 6.42 MB -BaekjoonHub

---
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(vector<int> numLog) {
+    string answer = "";
+    for (int i = 1; i < numLog.size(); i++) {
+        int diff = numLog[i] - numLog[i - 1];
+        if (diff == 1) {
+            answer += "w";
+        } else if (diff == -1) {
+            answer += "s";
+        } else if (diff == 10) {
+            answer += "d";
+        } else {
+            answer += "a";
+        }
+    }
+    return answer;
+}
