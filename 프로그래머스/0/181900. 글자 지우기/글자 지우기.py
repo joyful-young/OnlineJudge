b616ebd44572499b0d64bf7a6a6afe84206f1a98
JaeYoung Park <119635748+joyful-young@users.noreply.github.com>
2025-03-10 08:29:26 +0900

    [level 0] Title: 글자 지우기, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181900. 글자 지우기/글자 지우기.py
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.py
@@ -1,4 +1,4 @@
 def solution(my_string, indices):
     idx_set = set(indices)
-    answer = [my_string[idx] if idx not in idx_set else '' for idx in range(len(my_string))]
+    answer = [my_string[idx] for idx in range(len(my_string)) if idx not in idx_set]
     return ''.join(answer)
