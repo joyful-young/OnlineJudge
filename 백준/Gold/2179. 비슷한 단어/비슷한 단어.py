bb4830fc0ebd2372621735ad5bdf75d9586850d4
JaeYoung Park <119635748+joyful-young@users.noreply.github.com>
2025-03-18 22:39:52 +0900

    [Gold IV] Title: 비슷한 단어, Time: 52 ms, Memory: 34456 KB -BaekjoonHub

--- 백준/Gold/2179. 비슷한 단어/비슷한 단어.py
+++ 백준/Gold/2179. 비슷한 단어/비슷한 단어.py
@@ -10,30 +10,26 @@ def prefix_len(word1, word2):
     return min_length
 
 
-
 N = int(input())
 words = [input().rstrip() for _ in range(N)]
-sorted_words = sorted([[words[i], i] for i in range(N)])
+sorted_idxs = sorted(range(N), key=lambda i: words[i])
 
 
-ans = prefix_len(sorted_words[0][0], sorted_words[1][0])
-prefix_dict = {} if ans == 0 else {sorted_words[0][0][:ans]: [sorted_words[0][1], sorted_words[1][1]]}
+ans = prefix_len(words[sorted_idxs[0]], words[sorted_idxs[1]])
+prefix_dict = {} if ans == 0 else {words[sorted_idxs[0]][:ans]: [sorted_idxs[0], sorted_idxs[1]]}
 
-idx = 2
-while idx < N:
-    p_len = prefix_len(sorted_words[idx - 1][0], sorted_words[idx][0])
-    
+for i in range(2, N):
+    p_len = prefix_len(words[sorted_idxs[i - 1]], words[sorted_idxs[i]])
     if p_len > ans:
         ans = p_len
-        prefix_dict = {sorted_words[idx][0][:p_len]: [sorted_words[idx - 1][1], sorted_words[idx][1]]}
+        prefix_dict = {words[sorted_idxs[i]][:p_len]: [sorted_idxs[i - 1], sorted_idxs[i]]}
     elif p_len == ans:
         # 연이어서 접두사가 같은 경우
-        if sorted_words[idx][0][:p_len] in prefix_dict:
-            prefix_dict[sorted_words[idx][0][:p_len]].append(sorted_words[idx][1])
+        if words[sorted_idxs[i]][:p_len] in prefix_dict:
+            prefix_dict[words[sorted_idxs[i]][:p_len]].append(sorted_idxs[i])
         # 글자가 다른데 접두사 길이가 같은 경우
         else:
-            prefix_dict[sorted_words[idx][0][:p_len]] = [sorted_words[idx - 1][1], sorted_words[idx][1]]
-    idx += 1
+            prefix_dict[words[sorted_idxs[i]][:p_len]] = [sorted_idxs[i - 1], sorted_idxs[i]]
 
 word_idx_lists = [sorted(idx_list) for idx_list in prefix_dict.values()]
 if len(word_idx_lists) > 1:
@@ -41,4 +37,3 @@ if len(word_idx_lists) > 1:
     
 print(words[word_idx_lists[0][0]])
 print(words[word_idx_lists[0][1]])
-    
