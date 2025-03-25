68f6c558d65ad913457c3e46039868f880a01184
JaeYoung Park <119635748+joyful-young@users.noreply.github.com>
2025-03-25 11:35:03 +0900

    [level 2] Title: 두 원 사이의 정수 쌍, Time: 975.29 ms, Memory: 9.45 MB -BaekjoonHub

--- 프로그래머스/2/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.py
+++ 프로그래머스/2/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.py
@@ -1,8 +1,13 @@
 def solution(r1, r2):
+    """
+    1사분면(y축 포함, x축 제외)에서 점 개수 구해서 4배
+    0 <= x < r2, 0 < y <= r2
+    max(1, (r1)^2 - x^2) <= y^2 <= (r2)^2 - x^2
+    """
     answer = 0
     for x in range(0, r2):
-        temp = max(0, r1 * r1 - x * x) ** 0.5
-        min_y = max(1, int(temp)) if int(temp) == temp else int(temp) + 1
+        temp = max(1, r1 * r1 - x * x) ** 0.5
+        min_y = int(temp) if int(temp) == temp else int(temp) + 1
         
         max_y = int((r2 * r2 - x * x) ** 0.5)
         answer += max_y - min_y + 1
