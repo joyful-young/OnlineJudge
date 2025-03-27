15d403c589c85643d3088531c2458cf72a3d3ab5
JaeYoung Park <119635748+joyful-young@users.noreply.github.com>
2025-03-27 09:33:19 +0900

    [Gold IV] Title: 뮤탈리스크, Time: 64 ms, Memory: 34952 KB -BaekjoonHub

--- 백준/Gold/12869. 뮤탈리스크/뮤탈리스크.py
+++ 백준/Gold/12869. 뮤탈리스크/뮤탈리스크.py
@@ -3,22 +3,27 @@ from collections import deque
 N = int(input())
 scv = list(map(int, input().split())) + [0] * (3 - N)
 scv.sort(reverse=True)
+scv = tuple(scv)
 
-damages = [(-9, -3, -1), (-9, -1, -3), (-3, -9, -1), (-3, -1, -9), (-1, -9, -3), (-1, -3, -9)]
-dp = {}
+damages = [
+    (-9, -3, -1), (-9, -1, -3), 
+    (-3, -9, -1), (-3, -1, -9), 
+    (-1, -9, -3), (-1, -3, -9)
+]
+visited = {}
 def bfs():
-    q = deque([tuple(scv)])
-    dp[tuple(scv)] = 0
+    q = deque([scv])
+    visited[scv] = 0
     while q:
         now_scv = q.popleft()
         if now_scv == (0, 0, 0):
-            return dp[(0, 0, 0)]
+            return visited[(0, 0, 0)]
 
         for damage in damages:
             nxt_scv = tuple(sorted([max(now_scv[i] + damage[i], 0) for i in range(3)], reverse=True))
             
-            if not nxt_scv in dp:
-                dp[nxt_scv] = dp[now_scv] + 1
+            if not nxt_scv in visited:
+                visited[nxt_scv] = visited[now_scv] + 1
                 q.append(nxt_scv)
 
 print(bfs())
