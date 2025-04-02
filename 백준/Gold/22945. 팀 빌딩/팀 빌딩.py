33cba6830ab3c44f3d22a115b29a95acef19d969
JaeYoung Park <119635748+joyful-young@users.noreply.github.com>
2025-04-02 09:51:07 +0900

    [Gold IV] Title: 팀 빌딩, Time: 84 ms, Memory: 42168 KB -BaekjoonHub

--- 백준/Gold/22945. 팀 빌딩/팀 빌딩.py
+++ 백준/Gold/22945. 팀 빌딩/팀 빌딩.py
@@ -1,16 +1,26 @@
 N = int(input())
 developers = list(map(int, input().split()))
 
-left = 0
-right = N - 1
-max_v = 0
+small, big = sorted([0, N - 1], key=lambda x: developers[x])
+max_v = (N - 2) * developers[small]
 
-while left < right:
-    max_v = max(max_v, (right - left - 1) * min(developers[left], developers[right]))
+while small != big:
+    # 능력치가 더 작은 쪽을 옮겨봐야 함
+    nxt = small
+    if small < big:    # 능력치 낮은 게 더 왼쪽 인덱스. 오른쪽으로
+        while nxt < big and developers[small] >= developers[nxt]:
+            nxt += 1
+    else:              # 능력치 낮은 게 더 오른쪽 인덱스. 왼쪽으로
+        while big < nxt and developers[small] >= developers[nxt]:
+            nxt -= 1
 
-    if developers[left] < developers[right]:
-        left += 1
+    # nxt는 big과 같아졌거나 이전 A, B 능력치 최솟값보다 큰 값을 갖거나
+    if developers[nxt] <= developers[big]:
+        small = nxt
     else:
-        right -= 1
+        small = big
+        big = nxt
+
+    max_v = max(max_v, (abs(big - small) - 1) * developers[small])
 
 print(max_v)
