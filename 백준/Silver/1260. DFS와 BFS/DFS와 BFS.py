94f304cadd7f4ad6ccddd8d3b517792de052f665
JaeYoung Park <119635748+joyful-young@users.noreply.github.com>
2025-04-06 22:10:17 +0900

    [Silver II] Title: DFS와 BFS, Time: 64 ms, Memory: 35384 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,41 +1,50 @@
-# 재귀
-def dfs(v, visited):
-    visited[v] = 1
-    print(v, end=' ')
-    for w in adjL[v]:
-        if not visited[w]:
-            dfs(w, visited)
+import sys
+from collections import deque
+input = sys.stdin.readline
 
-
-def bfs(v, visited):
-    # 시작점 인큐
-    queue = [v]
-    visited[v] = 1
-
-    while queue:
-        v = queue.pop(0)
-        print(v, end=' ')
-        # 현재 정점에 인접한 정점 중 방문하지 않았던 곳 방문
-        for w in adjL[v]:
-            if not visited[w]:
-                queue.append(w)
-                visited[w] = 1
-        # 인접한 곳 다 방문했으면 처음으로 돌아가서 큐에서 정점 꺼내기
-
-
-N, M, V = map(int, input().split())     # 정점 개수, 간선 개수, 탐색 시작 정점
-adjL = [[] for _ in range(N + 1)]   # 정점 번호 1 ~ N
+N, M, V = map(int, input().split())
+adjL = [[] for _ in range(N + 1)]
 for _ in range(M):
-    v1, v2 = map(int, input().split())
-    adjL[v1].append(v2)
-    adjL[v2].append(v1)
-
-for i in range(N + 1):
-    adjL[i].sort()
+    v, w = map(int, input().split())
+    adjL[v].append(w)
+    adjL[w].append(v)
 
 
-visited = [0 for _ in range(N + 1)]
-dfs(V, visited)
-print()
-visited = [0 for _ in range(N + 1)]
-bfs(V, visited)
+def dfs(start):
+    result = []
+    stack = [start]
+    visited = [0 for _ in range(N + 1)]
+
+    while stack:
+        v = stack.pop()
+        if visited[v]:
+            continue
+
+        visited[v] = 1
+        result.append(v)
+        for w in sorted(adjL[v], reverse=True):    # 번호 큰 것부터 스택에 넣기
+            if not visited[w]:
+                stack.append(w)
+                
+    return result
+
+
+def bfs(start):
+    result = []
+    q = deque([start])
+    visited = [0 for _ in range(N + 1)]
+    visited[start] = 1
+
+    while q:
+        v = q.popleft()
+        result.append(v)
+
+        for w in sorted(adjL[v]):
+            if not visited[w]:
+                visited[w] = 1
+                q.append(w)
+                
+    return result
+
+print(*dfs(V))
+print(*bfs(V))
