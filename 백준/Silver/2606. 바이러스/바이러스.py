25a1a8b6ed355c447908ffb2561242412161d4a6
JaeYoung Park <119635748+joyful-young@users.noreply.github.com>
2025-04-07 09:02:53 +0900

    [Silver III] Title: 바이러스, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,38 +1,30 @@
-# 백준 2606. 바이러스
-# 재귀 말고 스택이랑 반복으로 풀어보기
+import sys
+input = sys.stdin.readline
 
-computers = int(input())
-link = int(input())
+V = int(input())
+E = int(input())
+adjL = [[] for _ in range(V + 1)]    # 1번 ~ V번
+for _ in range(E):
+    v, w = map(int, input().split())
+    adjL[v].append(w)
+    adjL[w].append(v)
 
-# 컴퓨터 간 연결 관계를 나타내는 리스트
-graph = [[] for _ in range(computers + 1)]
+def dfs(start):
+    stack = [start]
+    visited = [0 for _ in range(V + 1)]
 
-for i in range(link):
-    node1, node2 = map(int, input().split())
-    graph[node1].append(node2)
-    graph[node2].append(node1)
+    while stack:
+        v = stack.pop()
 
-# 현재 방문 중인 노드를 넣을 스택
-stack = [1]
+        if visited[v] == 1:
+            continue
 
-visited = [False] * (computers + 1)     # 방문 여부
-visited[1] = True
+        visited[v] = 1
 
-# 1번부터 시작해 방문하는 노드 수
-cnt = 0
+        for w in adjL[v]:
+            if visited[w] == 0:
+                stack.append(w)
 
-# 스택이 비지 않았으면 계속
-while stack:
-    node = stack.pop()
-    # 현재 노드에 연결된 노드들에 대해 반복
-    for next_node in graph[node]:
-        # 방문한 적 없는 곳이면
-        if not visited[next_node]:
-            # 방문
-            visited[next_node] = True
-            # 방문 횟수 추가
-            cnt += 1
-            # 그 노드로 이동
-            stack.append(next_node)
+    return sum(visited) - 1
 
-print(cnt)
+print(dfs(1))
