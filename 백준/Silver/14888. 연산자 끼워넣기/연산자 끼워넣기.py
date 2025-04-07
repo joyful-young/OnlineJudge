239f99e367689ae2cbaf5da5dd492af1fe081991
JaeYoung Park <119635748+joyful-young@users.noreply.github.com>
2025-04-07 20:29:32 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 68 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -4,51 +4,44 @@ input = sys.stdin.readline
 N = int(input())
 A = list(map(int, input().split()))
 plus, sub, mul, div = map(int, input().split())
-operators = [0] * plus + [1] * sub + [2] * mul + [3] * div
-visited = [0 for _ in range(N - 1)]
-perm = [-1 for _ in range(N - 1)]
+operators = {
+    0: plus,
+    1: sub,
+    2: mul,
+    3: div
+}
 
 max_value = -1000000000
 min_value = 1000000000
+    
 
-def calc(oper_perm, number):
-    for i in range(N - 1):
-        if oper_perm[i] == 0:
-            number += A[i + 1]
-        elif oper_perm[i] == 1:
-            number -= A[i + 1]
-        elif oper_perm[i] == 2:
-            number *= A[i + 1]
-        else:
-            if number < 0:
-                number = -((-number) // A[i + 1])
-            else:
-                number //= A[i + 1]
-    return number
-
-
-def get_perm(x, k):
-    """
-    perm[x] 값 정하기
-    """
+def calc(x, k, current):
     global max_value
     global min_value
-    
+
     if x == k:
-        temp = calc(perm, A[0])
-        max_value = max(max_value, temp)
-        min_value = min(min_value, temp)
+        max_value = max(max_value, current)
+        min_value = min(min_value, current)
         return
 
-    for i in range(N - 1):
-        if visited[i] == 1:
+    for oper, cnt in operators.items():
+        if cnt == 0:
             continue
 
-        visited[i] = 1
-        perm[x] = operators[i]
-        get_perm(x + 1, k)
-        visited[i] = 0
+        operators[oper] -= 1
+        if oper == 0:
+            calc(x + 1, k, current + A[x + 1])
+        elif oper == 1:
+            calc(x + 1, k, current - A[x + 1])
+        elif oper == 2:
+            calc(x + 1, k, current * A[x + 1])
+        else:
+            if current < 0:
+                calc(x + 1, k, -((-current) // A[x + 1]))
+            else:
+                calc(x + 1, k, current // A[x + 1])
+        operators[oper] += 1
 
-get_perm(0, N - 1)
+calc(0, N - 1, A[0])
 print(max_value)
 print(min_value)
