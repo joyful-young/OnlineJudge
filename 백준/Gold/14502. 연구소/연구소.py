7412b39196c6db4b79d0fb301606c38e142f8b91
JaeYoung Park <119635748+joyful-young@users.noreply.github.com>
2025-04-09 08:54:04 +0900

    [Gold IV] Title: 연구소, Time: 1304 ms, Memory: 34992 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -2,50 +2,44 @@ import sys
 from collections import deque
 input = sys.stdin.readline
 
+# 방향 벡터 (상, 하, 좌, 우)
 di = [-1, 1, 0, 0]
 dj = [0, 0, -1, 1]
 
 def spread(empty_place_cnt):
     global safety_zone
-    after_build_wall = [[arr[i][j] for j in range(M)] for i in range(N)]
-    visited = [[0 for _ in range(M)] for _ in range(N)]
-    for r, c in virus:
-        visited[r][c] = 1
+    after_build_wall = [row[:] for row in arr]
     q = deque(virus)
+
     while q:
         xi, xj = q.popleft()
-
-        for i in range(4):
-            ni, nj = xi + di[i], xj + dj[i]
-            if 0 <= ni < N and 0 <= nj < M and after_build_wall[ni][nj] == 0 and visited[ni][nj] == 0:
+        for d in range(4):
+            ni, nj = xi + di[d], xj + dj[d]
+            if 0 <= ni < N and 0 <= nj < M and after_build_wall[ni][nj] == 0:
+                after_build_wall[ni][nj] = 2
                 empty_place_cnt -= 1
                 if empty_place_cnt < safety_zone:
-                    return empty_place_cnt
-                visited[ni][nj] = 1
-                after_build_wall[ni][nj] = 2
+                    return  # 더 이상 최댓값 갱신 불가, 가지치기
                 q.append((ni, nj))
 
-    return empty_place_cnt
+    safety_zone = max(safety_zone, empty_place_cnt)
 
 def build_wall(n, r, s):
-    # n: 빈 공간 개수, r: 세워야 할 벽의 수
-    # s: empty 배열에서 선택 가능한 최초 요소 인덱스
-    global safety_zone
     if r == 0:
-        # 벽 모두 세운 후
-        safety_zone = max(safety_zone, spread(n - 3))
+        spread(n - 3)
         return
-
     for i in range(s, n - r + 1):
         wi, wj = empty[i]
         arr[wi][wj] = 1
         build_wall(n, r - 1, i + 1)
         arr[wi][wj] = 0
 
+# 입력 처리
 N, M = map(int, input().split())
 arr = [0 for _ in range(N)]
 empty = []
 virus = []
+
 for i in range(N):
     arr[i] = list(map(int, input().split()))
     for j in range(M):
@@ -57,4 +51,3 @@ for i in range(N):
 safety_zone = 0
 build_wall(len(empty), 3, 0)
 print(safety_zone)
-
