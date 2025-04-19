bb9034ca06082a4c50cd399d956ad03935cff522
JaeYoung Park <119635748+joyful-young@users.noreply.github.com>
2025-04-20 08:56:09 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 388 ms, Memory: 66532 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -1,39 +1,32 @@
-# 백준 1197. 최소 스패닝 트리
 import sys
+from heapq import heappush, heappop
 input = sys.stdin.readline
 
-
-def find_set(x):
-    while rep[x] != x:
-        x = rep[x]
-    return x
-
-
-def union(x, y):
-    rep[find_set(y)] = find_set(x)
-
-
 V, E = map(int, input().split())
-graph = []
-
+adjL = [[] for _ in range(V + 1)]
 for _ in range(E):
-    A, B, C = map(int, input().split())
-    graph.append([C, A, B])
+    a, b, c = map(int, input().split())
+    adjL[a].append((b, c))
+    adjL[b].append((a, c))
 
-graph.sort()
+def prim():
+    hq = []
+    heappush(hq, (0, 1))
+    visited = [0 for _ in range(V + 1)]
 
-# make-set
-rep = [i for i in range(V + 1)]     # 정점 번호 1 ~ V번
+    total = 0
+    
+    while hq:
+        cost, v = heappop(hq)
+        if visited[v] == 1:
+            continue
 
-s = 0       # 가중치 합
-cnt = 0
-for c, a, b in graph:       # 가중치가 작은 것부터
-    if find_set(a) != find_set(b):      # 사이클을 이루지 않으면
-        s += c      # 가중치 더하고
-        union(a, b)     # 두 집합 합치고
-        cnt += 1        # 선택한 간선 수 증가
+        visited[v] = 1
+        total += cost
+        
+        for w, v_to_w in adjL[v]:
+            if visited[w] == 0:
+                heappush(hq, (v_to_w, w))
+    return total
 
-        if cnt == V - 1:        # 간선을 모두 선택. MST 완성
-            break
-
-print(s)
+print(prim())
