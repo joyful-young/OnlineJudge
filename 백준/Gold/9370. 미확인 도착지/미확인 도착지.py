d08998dbd985a6a94971fb47e869f730eaa08d45
JaeYoung Park <119635748+joyful-young@users.noreply.github.com>
2025-04-24 12:18:08 +0900

    [Gold II] Title: 미확인 도착지, Time: 348 ms, Memory: 46772 KB -BaekjoonHub

---
+++ 백준/Gold/9370. 미확인 도착지/미확인 도착지.py
@@ -0,0 +1,51 @@
+import sys
+from heapq import heappush, heappop
+input = sys.stdin.readline
+
+MAX_D = 2000 * 1000
+def dijkstra(start, graph, nodes):
+    dist = [MAX_D for _ in range(nodes + 1)]
+    dist[start] = 0
+    
+    hq = []
+    heappush(hq, (0, start))
+    while hq:
+        c, v = heappop(hq)
+
+        if dist[v] < c:
+            continue
+
+        for w, v_to_w in graph[v]:
+            s_v_w = dist[v] + v_to_w
+            if s_v_w < dist[w]:
+                dist[w] = s_v_w
+                heappush(hq, (s_v_w, w))
+    return dist
+
+    
+T = int(input())
+for _ in range(T):
+    n, m, t = map(int, input().split())
+    s, g, h = map(int, input().split())
+
+    adjL = [[] for _ in range(n + 1)]
+    g_to_h = MAX_D
+    for _ in range(m):
+        a, b, d = map(int, input().split())
+        adjL[a].append((b, d))
+        adjL[b].append((a, d))
+        if {a, b} == {g, h}:
+            g_to_h = d
+
+    from_s = dijkstra(s, adjL, n)
+    from_h = dijkstra(h, adjL, n)
+    from_g = dijkstra(g, adjL, n)
+
+    candidates = [int(input()) for _ in range(t)]
+    ans = []
+    for e in candidates:
+        through_g_h = min(from_s[h] + g_to_h + from_g[e], from_s[g] + g_to_h + from_h[e])
+        if from_s[e] == through_g_h:
+            ans.append(e)
+    ans.sort()
+    print(*ans)
