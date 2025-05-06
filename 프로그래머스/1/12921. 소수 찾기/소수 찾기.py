8c28de67870559f1bf393cab4d373e7ca4468d3a
JaeYoung Park <119635748+joyful-young@users.noreply.github.com>
2025-05-06 21:28:22 +0900

    [level 1] Title: 소수 찾기, Time: 412.32 ms, Memory: 115 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.py
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.py
@@ -1,24 +1,7 @@
 def solution(n):
-    numbers = [True for i in range(1, n + 1)]
-    for num in range(1, n + 1):
-        if not numbers[num - 1]:
-            continue
-            
-        if is_prime(num):
-            num += num
-            while num <= n:
-                numbers[num - 1] = False
-                num += num
-        else:
-            numbers[num - 1] = False
-    return numbers.count(True)
+    num=set(range(2,n+1))
 
-
-def is_prime(num):
-    if num <= 1:
-        return False
-    
-    for x in range(2, int(num ** 0.5) + 1):
-        if num % x == 0:
-            return False
-    return True
+    for i in range(2,n+1):
+        if i in num:
+            num-=set(range(2*i,n+1,i))
+    return len(num)
