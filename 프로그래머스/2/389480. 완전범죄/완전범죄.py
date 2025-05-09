ef16c49defc3daded0a543b5ead35e933fe5bd78
JaeYoung Park <119635748+joyful-young@users.noreply.github.com>
2025-05-10 05:43:00 +0900

    [level 2] Title: 완전범죄, Time: 1.99 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/2/389480. 완전범죄/완전범죄.py
+++ 프로그래머스/2/389480. 완전범죄/완전범죄.py
@@ -1,26 +1,18 @@
+MAX = 40 * 3
+
 def solution(info, n, m):
-    """
-    되도록 B가 훔쳐야
-    B 흔적 허용치 넘으면 A도 훔쳐야
-    A가 물건을 훔치면 B의 흔적이 줄어듦
-    """
     N = len(info)
-    all_b = sum([info[i][1] for i in range(N)])     # B가 다 훔칠 경우
+    # dp[i][j]: 물건 i까지 봤을 때 B의 흔적 개수가 j일 때의 A의 누적 흔적 개수
+    dp = [[MAX for _ in range(m)] for _ in range(N + 1)]
+    dp[0][0] = 0
     
-    if all_b < m:
-        return 0    # B가 모두 훔치는 게 가능할 때
+    for i in range(1, N + 1):
+        a, b = info[i - 1]
+        for j in range(m):
+            dp[i][j] = min(dp[i][j], dp[i - 1][j] + a)
+            
+            if j + b < m:
+                dp[i][j + b] = min(dp[i][j + b], dp[i - 1][j])
     
-    goal = all_b - m + 1    # 줄어들어야 하는 B의 흔적(goal 이상은 줄어들어야)
-    
-    # dp[x]: A 흔적이 x일 때 B 흔적이 줄어드는 양의 최댓값
-    dp = [-1 for _ in range(n)]  # 인덱스: A의 흔적 0 ~ (n - 1)
-    dp[0] = 0
-    for a, b in info:
-        for x in range(n - 1, a - 1, -1):
-            if dp[x - a] != -1:
-                dp[x] = max(dp[x], dp[x - a] + b)
-    # print(dp)
-    for idx, value in enumerate(dp):
-        if value >= goal:
-            return idx
-    return -1
+    answer = min(dp[N][i] for i in range(m))
+    return answer if answer < n else -1
