395323845eb7cccc3df98193a88746f599c08e19
JaeYoung Park <119635748+joyful-young@users.noreply.github.com>
2025-05-15 13:47:30 +0900

    [level 3] Title: 이중우선순위큐, Time: 99.36 ms, Memory: 23.1 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -17,36 +17,23 @@ def solution(operations):
 
         if n == 1:
             # 최댓값 삭제
-            maximum = get_max(max_heap, deleted)
-            
-            if maximum:
-                deleted.add(maximum[1])
+            pop_invalid_v(max_heap, deleted)
+            if max_heap:
+                deleted.add(heappop(max_heap)[1])
         else:
             # 최솟값 삭제
-            minimum = get_min(min_heap, deleted)
-            
-            if minimum:
-                deleted.add(minimum[1])
-    
-    minimum = get_min(min_heap, deleted)
-    if not minimum:
+            pop_invalid_v(min_heap, deleted)
+            if min_heap:
+                deleted.add(heappop(min_heap)[1])
+                
+    pop_invalid_v(min_heap, deleted)
+    pop_invalid_v(max_heap, deleted)
+    if not min_heap:
         return [0, 0]
-    
-    deleted.add(minimum[1])
-    maximum = get_max(max_heap, deleted)
-    if not maximum:
-        return [minimum[0], minimum[0]]
-    
-    return [-maximum[0], minimum[0]]
+    return [-max_heap[0][0], min_heap[0][0]]
 
 
-def get_min(min_h, deleted):
-    while min_h and min_h[0][1] in deleted:
-        heappop(min_h)
-    return heappop(min_h) if min_h else None
-
-
-def get_max(max_h, deleted):
-    while max_h and max_h[0][1] in deleted:
-        heappop(max_h)
-    return heappop(max_h) if max_h else None
+def pop_invalid_v(hq, deleted):
+    while hq and hq[0][1] in deleted:
+        heappop(hq)
+        
