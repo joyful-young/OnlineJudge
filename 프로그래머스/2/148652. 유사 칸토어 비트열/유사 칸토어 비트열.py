5977112914e2cb5cf44198e0e8d87623eb5046f7
JaeYoung Park <119635748+joyful-young@users.noreply.github.com>
2025-05-16 22:50:29 +0900

    [level 2] Title: 유사 칸토어 비트열, Time: 114.69 ms, Memory: 9.17 MB -BaekjoonHub

---
+++ 프로그래머스/2/148652. 유사 칸토어 비트열/유사 칸토어 비트열.py
@@ -0,0 +1,34 @@
+FIRST = "11011"
+
+def solution(n, l, r):
+    return count_one(n, l - 1, r - 1)
+
+
+def count_one(n, left, right):  # left, right은 0-base idx
+    if n == 1:
+        answer = 0
+        for i in range(left, right + 1):
+            if FIRST[i] == "1":
+                answer += 1
+        return answer
+    
+    five_pow = 5 ** (n - 1)
+    lq, lr = divmod(left, five_pow)
+    rq, rr = divmod(right, five_pow)
+    
+    if lq == rq:
+        if lq == 2:
+            return 0
+        return count_one(n - 1, lr, rr)
+    
+    if lq == 2:
+        answer = count_one(n - 1, 0, rr)
+    elif rq == 2:
+        answer = count_one(n - 1, lr, five_pow - 1)
+    else:
+        answer = count_one(n - 1, lr, five_pow - 1) + count_one(n - 1, 0, rr)
+
+    for i in range(lq + 1, rq):
+        if i != 2:
+            answer += count_one(n - 1, 0, five_pow - 1)
+    return answer
