b307abe0b0f762cb18e2b08ec2374396eae41719
JaeYoung Park <119635748+joyful-young@users.noreply.github.com>
2025-05-16 23:02:55 +0900

    [level 2] Title: 유사 칸토어 비트열, Time: 8053.00 ms, Memory: 9.12 MB -BaekjoonHub

--- 프로그래머스/2/148652. 유사 칸토어 비트열/유사 칸토어 비트열.py
+++ 프로그래머스/2/148652. 유사 칸토어 비트열/유사 칸토어 비트열.py
@@ -1,30 +1,11 @@
-FIRST = "11011"
-
 def solution(n, l, r):
-    return count_one(n, l - 1, r - 1)
+    answer = r - l + 1
+    for idx in range(l - 1, r):     # 0-base idx
+        while idx > 0:
+            idx, r = divmod(idx, 5)
+            
+            if r == 2 or idx == 2:      # 00000 구간 or 11011 에서 0
+                answer -= 1
+                break
 
-
-def count_one(n, left, right):  # left, right은 0-base idx
-    if n == 1:
-        return FIRST[left:right + 1].count("1")
-    
-    five_pow = 5 ** (n - 1)
-    lq, lr = divmod(left, five_pow)
-    rq, rr = divmod(right, five_pow)
-    
-    if lq == rq:
-        if lq == 2:
-            return 0
-        return count_one(n - 1, lr, rr)
-    
-    if lq == 2:
-        answer = count_one(n - 1, 0, rr)
-    elif rq == 2:
-        answer = count_one(n - 1, lr, five_pow - 1)
-    else:
-        answer = count_one(n - 1, lr, five_pow - 1) + count_one(n - 1, 0, rr)
-
-    for i in range(lq + 1, rq):
-        if i != 2:
-            answer += count_one(n - 1, 0, five_pow - 1)
     return answer
