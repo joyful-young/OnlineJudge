3ca8a39ffc9d1e7220af910d5e5f7a70da12f92c
JaeYoung Park <119635748+joyful-young@users.noreply.github.com>
2025-05-22 08:57:50 +0900

    [level 3] Title: 등굣길, Time: 3.59 ms, Memory: 9.51 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.py
+++ 프로그래머스/3/42898. 등굣길/등굣길.py
@@ -1,15 +1,14 @@
 DIV = 1000000007
 
 def solution(m, n, puddles):
-    # dp[i][j]: (j, i)로 가는 최단경로의 수 dp[i - 1][j] + dp[i][j - 1]
-    dp = [[0 for _ in range(m + 1)] for _ in range(n + 1)]
-    puddles_set = set(map(tuple, puddles))
+    puddles = set(map(tuple, puddles))
+    
+    dp = [[0] * (m + 1) for _ in range(n + 1)]
     dp[1][1] = 1
     for i in range(1, n + 1):
         for j in range(1, m + 1):
-            if (j, i) in puddles_set:
-                continue
-                
-            dp[i][j] += dp[i - 1][j] + dp[i][j - 1]
-            dp[i][j] %= DIV
+            if (j, i) not in puddles:
+                dp[i][j] += dp[i - 1][j] + dp[i][j - 1]
+                dp[i][j] %= DIV
+    print(dp)
     return dp[n][m]
