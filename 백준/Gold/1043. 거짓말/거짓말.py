d05e657f88b2b71eaccfa566280e1911db6d62da
JaeYoung Park <119635748+joyful-young@users.noreply.github.com>
2025-05-28 15:24:12 +0900

    [Gold IV] Title: 거짓말, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/1043. 거짓말/거짓말.py
+++ 백준/Gold/1043. 거짓말/거짓말.py
@@ -1,66 +1,48 @@
-# 백준 1043. 거짓말
 import sys
 input = sys.stdin.readline
 
 
-def find_set(x):
-    while rep[x] != x:
-        x = rep[x]
-    return x
+def find(x):
+    if rep[x] == x:
+        return x
+
+    rep[x] = find(rep[x])
+    return rep[x]
 
 
 def union(x, y):
-    rep[find_set(y)] = find_set(x)
+    rep_x, rep_y = find(x), find(y)
+
+    if rep_x < rep_y:
+        rep[rep_y] = rep_x
+    else:
+        rep[rep_x] = rep_y
 
 
 N, M = map(int, input().split())
-know_truth = list(map(int, input().split()))
-if know_truth[0] == 0:      # 진실 아는 사람 없으면
-    print(M)        # 모든 파티에서 과장된 얘기
-else:
-    know_truth = know_truth[1:]     # 진실 아는 사람 번호 리스트
+knowing = list(map(int, input().split()))
+parties = [list(map(int, input().split())) for _ in range(M)]
 
-    # make_set
-    rep = [i for i in range(N + 1)]     # 사람 번호 1부터 N
-    parties = []
-    # 같은 파티에 오는 사람들 집합으로 묶어서
-    for _ in range(M):      # M개의 파티
-        tmp = list(map(int, input().split()))
-        if tmp[0] == 1:     # 파티 오는 사람 1명이면 넘어감
-            parties.append(tmp[1:])
+
+if knowing[0] == 0:
+    print(M)
+else:
+    rep = [i for i in range(N + 1)]
+
+    for i in range(M):
+        if parties[i][0] <= 1:
             continue
 
-        # 한 파티 참석자 모두 같은 집합으로 묶기
-        cnt = tmp[0]        # 파티 참석자 수
-        people = tmp[1:]
-        parties.append(people)
-        for i in range(1, cnt):         # 같은 집합으로 묶기
-            union(people[0], people[i])
+        for j in range(1, parties[i][0]):
+            for k in range(j + 1, parties[i][0] + 1):
+                union(parties[i][j], parties[i][k])
 
-    # print(rep)
-
-    # 진실 아는 사람 포함된 집합은 제외하고
-    # know_truth에 있는 사람들과 대표 원소 같은 것은 다 제외
-    # 남은 집합들에 있는 사람들 모아서
-    lst = []
-    for person_num in range(1, N + 1):  # 1번 ~ N번 사람
-        rep_elem = find_set(person_num)
-        for know_person in know_truth:      # 진실 아는 사람이 포함된 집합의 대표원소
-            if rep_elem == find_set(know_person):   # 진실을 아는 사람과 같은 집합에 포함된 사람이면
-                break
-        else:   # 진실을 아는 사람과 같은 집합에 속하지 않을 경우
-            lst.append(person_num)      # 번호 넣기
-
-    # print(lst)
-    # print(parties)
-
-    # 파티마다 오는 사람 집합이 그 사람들 집합에 포함되면
-    # 그건 과장된 얘기 할 수 있는 파티
+    group = set(find(i) for i in knowing[1:])
     ans = 0
-    lst = set(lst)
     for party in parties:
-        party = set(party)
-        if party.issubset(lst):
+        for person in party[1:]:
+            if find(person) in group:
+                break
+        else:
             ans += 1
-
     print(ans)
