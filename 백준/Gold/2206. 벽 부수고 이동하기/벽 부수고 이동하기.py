47c1c3406835dc6cd610ee20439dc89ac01dc187
JaeYoung Park <119635748+joyful-young@users.noreply.github.com>
2025-07-28 14:48:09 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 2504 ms, Memory: 112912 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
@@ -9,31 +9,28 @@ DIR = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 N, M = map(int, input().split())
 arr = [input().rstrip() for _ in range(N)]
 
-visited = [[[False for _ in range(M)] for _ in range(N)] for _ in range(2)]
-dist = [[[N * M + 1 for _ in range(M)] for _ in range(N)] for _ in range(2)]
-dist[0][0][0] = 1
+MAX_D = N * M + 1
+visited = [[[MAX_D for _ in range(M)] for _ in range(N)] for _ in range(2)]
+visited[0][0][0] = 1
 
 
 def bfs(sr, sc):
     dq = deque([(sr, sc, 0)])
-    visited[0][sr][sc] = True
     
     while dq:
         r, c, broke = dq.popleft()
 
         if r == N - 1 and c == M - 1:
-            return dist[broke][r][c]
+            return visited[broke][r][c]
 
         for dr, dc in DIR:
             nr, nc = r + dr, c + dc
             if 0 <= nr < N and 0 <= nc < M:
-                if arr[nr][nc] == "0" and not visited[broke][nr][nc]:
-                    visited[broke][nr][nc] = True
-                    dist[broke][nr][nc] = dist[broke][r][c] + 1
+                if arr[nr][nc] == "0" and visited[broke][nr][nc] == MAX_D:
+                    visited[broke][nr][nc] = visited[broke][r][c] + 1
                     dq.append((nr, nc, broke))
-                elif arr[nr][nc] == "1" and broke == 0 and not visited[1][nr][nc]:
-                    visited[1][nr][nc] = True
-                    dist[1][nr][nc] = dist[0][r][c] + 1
+                elif arr[nr][nc] == "1" and broke == 0 and visited[1][nr][nc] == MAX_D:
+                    visited[1][nr][nc] = visited[0][r][c] + 1
                     dq.append((nr, nc, 1))
 
     return -1
