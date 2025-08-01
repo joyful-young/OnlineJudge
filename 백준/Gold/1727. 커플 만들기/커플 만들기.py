974e5dc7c8ceb73659661ba19f133fcd9b6a8c7e
JaeYoung Park <119635748+joyful-young@users.noreply.github.com>
2025-08-01 14:24:46 +0900

    [Gold II] Title: 커플 만들기, Time: 360 ms, Memory: 40632 KB -BaekjoonHub

--- 백준/Gold/1727. 커플 만들기/커플 만들기.py
+++ 백준/Gold/1727. 커플 만들기/커플 만들기.py
@@ -3,6 +3,28 @@ input = sys.stdin.readline
 
 
 MAX_DIFF = 1000000 * 1000
+
+
+def min_total_diff(smaller, larger):
+    s_len = len(smaller)
+    l_len = len(larger)
+
+    # dp[i][j]: i번, j번까지 고려했을 때 성격차 최소합
+    dp = [[MAX_DIFF] * (l_len + 1) for _ in range(s_len + 1)]
+    dp[0][0] = 0
+
+    for i in range(s_len + 1):
+        for j in range(l_len + 1):
+            if i < s_len and j < l_len:
+                diff = abs(smaller[i] - larger[j])
+                # i와 j를 커플로 만들 경우 고려
+                dp[i + 1][j + 1] = min(dp[i + 1][j + 1], dp[i][j] + diff)
+
+            if j < l_len:
+                dp[i][j + 1] = min(dp[i][j + 1], dp[i][j])
+    return dp[s_len][l_len]
+
+
 M, F = map(int, input().split())
 m_personality = list(map(int, input().split()))
 f_personality = list(map(int, input().split()))
@@ -12,26 +34,6 @@ m_personality.sort()
 f_personality.sort()
 
 if M <= F:
-    dp = [[MAX_DIFF] * (F + 1) for _ in range(M + 1)]
-    dp[0][0] = 0
-
-    for i in range(M + 1):
-        for j in range(F + 1):
-            if i < M and j < F:
-                diff = abs(m_personality[i] - f_personality[j])
-                dp[i + 1][j + 1] = min(dp[i + 1][j + 1], dp[i][j] + diff)
-            if j < F:
-                dp[i][j + 1] = min(dp[i][j + 1], dp[i][j])
-    print(dp[M][F])
+    print(min_total_diff(m_personality, f_personality))
 else:
-    dp = [[MAX_DIFF] * (M + 1) for _ in range(F + 1)]
-    dp[0][0] = 0
-
-    for i in range(F + 1):
-        for j in range(M + 1):
-            if i < F and j < M:
-                diff = abs(f_personality[i] - m_personality[j])
-                dp[i + 1][j + 1] = min(dp[i + 1][j + 1], dp[i][j] + diff)
-            if j < M:
-                dp[i][j + 1] = min(dp[i][j + 1], dp[i][j])
-    print(dp[F][M])
+    print(min_total_diff(f_personality, m_personality))
