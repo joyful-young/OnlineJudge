bbbfca57c5102da443051e3066b7bbb0f26fce1f
JaeYoung Park <119635748+joyful-young@users.noreply.github.com>
2025-08-06 19:31:36 +0900

    [Gold II] Title: 동전 분배, Time: 1104 ms, Memory: 111944 KB -BaekjoonHub

--- 백준/Gold/1943. 동전 분배/동전 분배.py
+++ 백준/Gold/1943. 동전 분배/동전 분배.py
@@ -1,28 +1,39 @@
-# 1943. 동전 분배
+import sys
+input = sys.stdin.readline
+
+
 T = 3
 for _ in range(T):
     N = int(input())
-    coin_pair_arr = [None for _ in range(N)]
+    coins = []
+    
     total = 0
-    for i in range(N):
-        coin_pair_arr[i] = list(map(int, input().split()))
-        total += coin_pair_arr[i][0] * coin_pair_arr[i][1]
+    for _ in range(N):
+        coin, cnt = map(int, input().split())
+        coins.append((coin, cnt))
+        total += coin * cnt
 
-    # print(coin_pair_arr)
-
-    if total % 2 == 1:
+    q, r = divmod(total, 2)
+    if r != 0:
         print(0)
         continue
 
-    half = total // 2
-    dp = [0 for _ in range(half + 1)]  # dp[i] 갖고 있는 동전으로 i원을 만드는 것이 가능한지
-    dp[0] = 1
+    dp = [False] * (q + 1)
+    dp[0] = True
 
-    for coin, cnt in coin_pair_arr:
-        for i in range(half, coin - 1, -1):
-            if dp[i - coin]:
-                for j in range(cnt):
-                    if i + coin * j <= half:
-                        dp[i + coin * j] = 1
+    for i in range(N):
+        coin, cnt = coins[i]
+
+        for j in range(q, coin - 1, -1):
+            if dp[j - coin]:
+                for k in range(cnt):
+                    if j + coin * k > q:
+                        break
                         
-    print(dp[half])
+                    dp[j + coin * k] = True
+
+        if dp[q]:
+            print(1)
+            break
+    else:
+        print(0)
