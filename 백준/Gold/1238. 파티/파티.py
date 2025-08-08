2a6cfd5d04119d600cd7fac208c10c0ac73700d3
JaeYoung Park <119635748+joyful-young@users.noreply.github.com>
2025-08-08 15:33:39 +0900

    [Gold III] Title: 파티, Time: 48 ms, Memory: 36532 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.py
+++ 백준/Gold/1238. 파티/파티.py
@@ -3,8 +3,6 @@ from heapq import heappush, heappop
 input = sys.stdin.readline
 
 N, M, X = map(int, input().split())
-MAX_DIST = 100000
-
 adjL = [[] for _ in range(N + 1)]
 rev_adjL = [[] for _ in range(N + 1)]
 for _ in range(M):
@@ -12,15 +10,17 @@ for _ in range(M):
     adjL[s].append((e, t))
     rev_adjL[e].append((s, t))
 
+
 def dijkstra(start, graph):
-    dist = [MAX_DIST for _ in range(N + 1)]
+    dist = [1000000] * (N + 1)
+    visited = [False] * (N + 1)
     dist[start] = 0
-    
-    q = []
-    heappush(q, (0, start))
-    while q:
-        # 출발점에서의 거리가 가장 짧은 노드
-        cost, v = heappop(q)
+    visited[start] = True
+
+    hq = []
+    heappush(hq, (0, start))
+    while hq:
+        cost, v = heappop(hq)
 
         if dist[v] < cost:
             continue
@@ -29,14 +29,10 @@ def dijkstra(start, graph):
             via_cost = cost + v_to_w
             if via_cost < dist[w]:
                 dist[w] = via_cost
-                heappush(q, (via_cost, w))
-
+                heappush(hq, (via_cost, w))
     return dist
 
-# X에서 각 마을로
 x_to_other = dijkstra(X, adjL)
-
-# 각 마을에서 X로. 역방향 그래프에서 X로부터의 최단거리 구함.
 other_to_x = dijkstra(X, rev_adjL)
 
 print(max([x_to_other[i] + other_to_x[i] for i in range(1, N + 1)]))
