c0556a94e98c61d952f48798554df9a6023e158b
JaeYoung Park <119635748+joyful-young@users.noreply.github.com>
2025-08-10 15:41:20 +0900

    [Gold II] Title: 1의 개수 세기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/9527. 1의 개수 세기/1의 개수 세기.py
+++ 백준/Gold/9527. 1의 개수 세기/1의 개수 세기.py
@@ -1,27 +1,30 @@
-# 9527. 1의 개수 세기
 A, B = map(int, input().split())
 
-def count(num):
-    ans = 0
-    while num > 0:
+n = 64
+# prefix_sum[i]: 2^(i) - 1 까지의 1의 개수. f(2^i - 1)
+prefix_sum = [0] * n
+for i in range(1, n):
+    prefix_sum[i] += 2 * prefix_sum[i - 1] + 2 ** (i - 1)
+
+
+def func(x):
+    ones = 0
+    while x > 0:
+        # x 이하의 2^(i) - 1 꼴 수 중 가장 큰 값이 되도록
+        # 2^(power) - 1
         power = 0
-        temp = num + 1
+        temp = x + 1
         while temp > 1:
             temp >>= 1
             power += 1
 
-        ans += acc_sum[power]
-        diff = num - (2 ** power - 1)
-        ans += diff
-        num = diff - 1
+        ones += prefix_sum[power]
+        # 나머지 2^(power) ~ x 까지 개수
+        remained = x - (2 ** power - 1)
+        # 그 숫자들의 맨 앞 1을 센다고 생각
+        ones += remained
+        # 그 숫자들은 각각 2^(power)로 나누어서 0 ~ x % (2^(power))를 구한다고 생각
+        x = remained - 1
+    return ones
 
-    return ans
-
-
-n = 64
-# 누적합 a_n = 2 * a_(n-1) + 2^(n-1)
-acc_sum = [0 for _ in range(n)]
-for i in range(1, n):
-    acc_sum[i] = 2 * acc_sum[i - 1] + 2 ** (i - 1)
-
-print(count(B) - count(A - 1))
+print(func(B) - func(A - 1))
