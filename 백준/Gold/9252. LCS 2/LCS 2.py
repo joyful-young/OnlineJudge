5c3f5136e0f40e139559a7fd146233c7f960f5a4
JaeYoung Park <119635748+joyful-young@users.noreply.github.com>
2025-08-16 17:33:44 +0900

    [Gold IV] Title: LCS 2, Time: 452 ms, Memory: 57628 KB -BaekjoonHub

--- 백준/Gold/9252. LCS 2/LCS 2.py
+++ 백준/Gold/9252. LCS 2/LCS 2.py
@@ -3,24 +3,28 @@ str2 = input()
 length1 = len(str1)
 length2 = len(str2)
 
-dp = [[[0, ""] for _ in range(length2 + 1)] for _ in range(length1 + 1)]
+dp = [[0] * (length2 + 1) for _ in range(length1 + 1)]
 
 for i in range(1, length1 + 1):
     for j in range(1, length2 + 1):
         if str1[i - 1] == str2[j - 1]:
-            dp[i][j][0] = dp[i - 1][j - 1][0] + 1
-            dp[i][j][1] = dp[i - 1][j - 1][1] + str1[i - 1]
-        
-        elif dp[i - 1][j][0] <= dp[i][j - 1][0]:
-            dp[i][j][0] = dp[i][j - 1][0]
-            dp[i][j][1] = dp[i][j - 1][1]
-            
+            dp[i][j] = dp[i - 1][j - 1] + 1
         else:
-            dp[i][j][0] = dp[i - 1][j][0]
-            dp[i][j][1] = dp[i - 1][j][1]
+            dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
 
-lcs_length = dp[length1][length2][0]
-print(lcs_length)
-if lcs_length != 0:
-    print(dp[length1][length2][1])
-    
+# LCS 문자열 찾기
+i, j = length1 - 1, length2 - 1
+lcs = []
+while i >= 0 and j >= 0:
+    if str1[i] == str2[j]:
+        lcs.append(str1[i])
+        i -= 1
+        j -= 1
+    elif dp[i][j + 1] >= dp[i + 1][j]:
+        i -= 1
+    else:
+        j -= 1
+
+print(dp[length1][length2])
+if dp[length1][length2] != 0:
+	print("".join(reversed(lcs)))
