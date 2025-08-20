6a9d990a3828cdd1d73bf23bb22c3d5735c11269
JaeYoung Park <119635748+joyful-young@users.noreply.github.com>
2025-08-20 11:32:53 +0900

    [Gold V] Title: 합분해, Time: 52 ms, Memory: 34456 KB -BaekjoonHub

--- 백준/Gold/2225. 합분해/합분해.py
+++ 백준/Gold/2225. 합분해/합분해.py
@@ -1,13 +1,15 @@
 N, K = map(int, input().split())
 
+MOD = 1_000_000_000
 dp = [[0] * (K + 1) for _ in range(N + 1)]
 for n in range(N + 1):
     dp[n][1] = 1
-
+    
 for k in range(1, K + 1):
-    for n in range(N + 1):
-        for i in range(n + 1):
-            dp[n][k] += dp[n - i][k - 1]
-            dp[n][k] %= 1000000000
+    dp[0][k] = 1
+
+for k in range(2, K + 1):
+    for n in range(1, N + 1):
+        dp[n][k] = (dp[n][k - 1] + dp[n - 1][k]) % MOD
 
 print(dp[N][K])
