70b3beb0c07b04048c7ff5fa9b2f508366581a20
JaeYoung Park <119635748+joyful-young@users.noreply.github.com>
2025-09-09 22:47:27 +0900

    [Gold III] Title: 소용돌이 예쁘게 출력하기, Time: 916 ms, Memory: 110956 KB -BaekjoonHub

--- 백준/Gold/1022. 소용돌이 예쁘게 출력하기/소용돌이 예쁘게 출력하기.py
+++ 백준/Gold/1022. 소용돌이 예쁘게 출력하기/소용돌이 예쁘게 출력하기.py
@@ -1,47 +1,60 @@
 import sys
 input = sys.stdin.readline
 
+# 소용돌이 방향. 우-상-좌-하
+# 상->좌일 때 거리 1 증가해야 함
+DIR = [(0, 1), (-1, 0), (0, -1), (1, 0)]
 
 r1, c1, r2, c2 = map(int, input().split())
+# 소용돌이 크기
+S = max(abs(r1), abs(c1), abs(r2), abs(c2))
 
 # 출력할 배열 크기
 R = r2 - r1 + 1
 C = c2 - c1 + 1
 
-# 배열 초기화
+# 출력할 배열
 arr = [[-1] * C for _ in range(R)]
 
-# arr[r][c]에 들어갈 숫자 계산
-def calc_number(r, c):
-    # 소용돌이 크기
-    k = max(abs(r), abs(c))
-    # 오른쪽 아래 꼭짓점 숫자
-    right_down = (2 * k + 1) ** 2
+
+def is_valid(nr, nc):
+    return 0 <= nr < R and 0 <= nc < C
+
+
+# 배열 채우기
+# 원점
+r, c = -r1, -c1
+num = 1
+if is_valid(r, c):
+    arr[r][c] = num
+num += 1
+
+size = -1
+d = 0
+l = 1
+
+max_v = 0
+while size < S:
+    # 직진
+    for _ in range(l):
+        r += DIR[d][0]
+        c += DIR[d][1]
+        
+        if is_valid(r, c):
+            arr[r][c] = num
+            max_v = num
+        num += 1
     
-    if r == k:
-        # 아랫변
-        return right_down - (k - c)
-    elif c == -k:
-        # 왼쪽 변
-        return right_down - 2 * k - (k - r)
-    elif r == -k:
-        # 윗변
-        return right_down - 4 * k - (k + c)
-    else:
-        # 오른쪽 변
-        return right_down - 6 * k - (k + r)
-
-max_n = 0
-for r in range(r1, r2 + 1):
-    for c in range(c1, c2 + 1):
-        v = calc_number(r, c)
-        arr[r - r1][c - c1] = v
-        if v > max_n:
-            max_n = v
-
-# 가장 긴 숫자 길이
-max_l = len(str(max_n))
-
+    # 방향 바꾸기
+    if d == 0:
+        size += 1
+    elif d == 1 or d == 3:
+        l += 1
+    
+    d = (d + 1) % 4
+    
+#TODO
+max_l = len(str(max_v))
 results = []
 for row in arr:
     # f"{v:>{max_l}}" v 값을 폭 max_l 공간에서 오른쪽 정렬(>)
