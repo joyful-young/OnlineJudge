a57ceefc3cfd2455b5bb1362d15e05c0cb5edd3c
JaeYoung Park <119635748+joyful-young@users.noreply.github.com>
2025-10-02 21:54:20 +0900

    [Gold III] Title: 감시, Time: 88 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/15683. 감시/감시.py
+++ 백준/Gold/15683. 감시/감시.py
@@ -1,60 +1,69 @@
 import sys
 input = sys.stdin.readline
 
-cctv_dir = {
-    1: [[(0, 1)], [(-1, 0)], [(0, -1)], [(1, 0)]],
-    2: [[(0, 1), (0, -1)], [(-1, 0), (1, 0)]],
-    3: [[(0, 1), (-1, 0)], [(-1, 0), (0, -1)], [(0, -1), (1, 0)], [(1, 0), (0, 1)]],
+D = {
+    1: [[(0, 1)], [(1, 0)], [(0, -1)], [(-1, 0)]],
+    2: [[(0, -1), (0, 1)], [(-1, 0), (1, 0)]],
+    3: [[(-1, 0), (0, 1)], [(0, 1), (1, 0)], [(1, 0), (0, -1)], [(0, -1), (-1, 0)]],
     4: [
-        [(0, 1), (-1, 0), (0, -1)],
-        [(-1, 0), (0, -1), (1, 0)],
-        [(0, -1), (1, 0), (0, 1)],
-        [(1, 0), (0, 1), (-1, 0)]
+        [(0, -1), (-1, 0), (0, 1)], 
+        [(-1, 0), (0, 1), (1, 0)], 
+        [(0, 1), (1, 0), (0, -1)],
+        [(1, 0), (0, -1), (-1, 0)]
     ],
-    5: [[(0, 1), (-1, 0), (0, -1), (1, 0)]]
+    5: [[(0, -1), (-1, 0), (0, 1), (1, 0)]]
 }
 
+
 def monitor(ty, r, c):
+    # (r, c)에 위치한 ty 종류의 cctv가 감시하는 영역
     monitored = []
-    for dir_list in cctv_dir[ty]:
-        temp = set()
-        for dr, dc in dir_list:
+    for directions in D[ty]:
+        tmp = set()
+        for dr, dc in directions:
             nr, nc = r + dr, c + dc
-            while 0 <= nr < N and 0 <= nc < M and arr[nr][nc] != 6:
-                if arr[nr][nc] == 0:
-                    temp.add((nr, nc))
+            while 0 <= nr < N and 0 <= nc < M and grid[nr][nc] != 6:
+                if grid[nr][nc] == 0:
+                    tmp.add((nr, nc))
                 nr += dr
                 nc += dc
-        monitored.append(temp)
+        monitored.append(tmp)
     return monitored
 
-def count_monitored_area(x, k, monitored_area_set):
-    global monitored_cnt
+
+def bt(x, k, current_monitored):
+    global max_v
     if x == k:
-        # 전체 cctv 방향 결정 완료
-        monitored_cnt = max(monitored_cnt, len(monitored_area_set))
+        # 감시되는 영역 세기
+        max_v = max(max_v, len(current_monitored))
         return
 
-    for area_set in monitored_area[x]:        
-        count_monitored_area(x + 1, k, monitored_area_set.union(area_set))
-    
+    # x번 cctv 방향 정해 감시 영역 확인
+    for area_set in monitored_area[x]:
+        bt(x + 1, k, current_monitored.union(area_set))
+
 
 N, M = map(int, input().split())
-arr = [0 for _ in range(N)]
-area = 0
+grid = [list(map(int, input().split())) for _ in range(N)]
+
+# 0인 공간 개수
+empty_area = 0
 cctv = []
 for i in range(N):
-    arr[i] = list(map(int, input().split()))
     for j in range(M):
-        if arr[i][j] == 0:
-            area += 1
+        if grid[i][j] == 0:
+            empty_area += 1
             continue
-        if arr[i][j] != 6:
-            cctv.append((arr[i][j], i, j))
 
-# 각 cctv에서 방향별 감시 영역 좌표 구하기
-# monitored_area[cctv 인덱스] = [방향별 감시 영역]
-monitored_area = [monitor(*c) for c in cctv]
-monitored_cnt = 0
-count_monitored_area(0, len(cctv), set())
-print(area - monitored_cnt)
+        if grid[i][j] != 6:
+            cctv.append((grid[i][j], i, j))
+
+# 각 cctv의 방향별 감시 영역 좌표 구하기
+monitored_area = [monitor(*elem) for elem in cctv]
+
+# 감시되는 영역 최대 개수
+max_v = 0
+bt(0, len(cctv), set())
+
+# 사각지대 최소 개수
+print(empty_area - max_v)
