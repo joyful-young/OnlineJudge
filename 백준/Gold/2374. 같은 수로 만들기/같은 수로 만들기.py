e6cbe628298f5811402edafa01f22faf43406e29
JaeYoung Park <119635748+joyful-young@users.noreply.github.com>
2025-12-08 22:01:50 +0900

    [Gold IV] Title: 같은 수로 만들기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Gold/2374. 같은 수로 만들기/같은 수로 만들기.py
@@ -0,0 +1,30 @@
+# 2374. 같은 수로 만들기
+import sys
+input = sys.stdin.readline
+
+
+N = int(input())
+numbers = [int(input()) for _ in range(N)]
+stack = []
+cnt = 0
+idx = 0
+while idx < N:
+    number = numbers[idx]
+    if stack and stack[-1] < number:
+        if len(stack) >= 2 and stack[-2] < number:
+            cnt += stack[-2] - stack[-1]
+            stack.pop()
+            continue
+        else:
+            cnt += number - stack[-1]
+            stack[-1] = number
+    elif not stack or stack[-1] > number:
+        stack.append(number)
+    
+    idx += 1
+
+while len(stack) >= 2:
+    cnt += stack[-2] - stack[-1]
+    stack.pop()
+
+print(cnt)
